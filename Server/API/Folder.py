81d1639d19c51f6228eb6a205a173472e9c02983
LRojon <loic.rojon@gmail.com>
2020-12-03 16:35:28 +0100

    API - Folder: GetFolderById, Fait

--- Server/API/Folder.py
+++ Server/API/Folder.py
@@ -1,4 +1,6 @@
+import json
 from flask import Flask, request, current_app, g, jsonify
+from flask.wrappers import Response
 from flask_restful import Resource
 
 from utils.DAO_Object import dao
@@ -6,7 +8,7 @@ from utils.DAO_Object import dao
 
 class Tree(Resource):
     def post(self, command):
-        # request.get_json(force=True)
+        request.get_json(force=True)
         if command == "getTree":
             type = request.json['type']
             if type == "Mob" or type == "Character" or type == "Map":
@@ -16,22 +18,38 @@ class Tree(Resource):
                 for row in rows:
                     tmp.append(Folder(
                         row[0],
-                        row[1]
+                        row[1],
+                        opened=True
                     ))
                 tmp = Arbo(tmp, type)
                 ret = []
                 for elem in tmp:
                     ret.append(elem.getJSON())
+                path = './file/tree/' + type + '.json'
+                file = open('../Client/' + path[2:], 'w+')
+                file.write(json.dumps(ret))
+                file.close()
                 return {"code": 200, "data": ret}
             else:
                 return {"code": 400, "message": "Bad Request: This type doesn't exist"}
+        elif command == "getFolderById":
+            rows = dao.select("SELECT * FROM Folder WHERE id=?",
+                              (request.json['id'],))
+            folder = []
+            type = ""
+            for row in rows:
+                folder.append(Folder(row[0], row[1]))
+                type = row[2]
+            folder = Arbo(folder, type)
+            return Response(response=json.dumps(folder[0].getJSON()), status=200, mimetype="application/json")
 
 
 class Folder:
-    def __init__(self, id, name, items=None):
+    def __init__(self, id, name, items=None, opened=False):
         self.id = id
         self.value = name
         self.items = items
+        self.opened = opened
 
     def getJSON(self):
         items = []
@@ -40,6 +58,7 @@ class Folder:
         return {
             'id': self.id,
             'value': self.value,
+            'opened': self.opened,
             'items': items
         }
 
@@ -56,7 +75,7 @@ class Item:
         }
 
 
-def Arbo(folders, type):
+def Arbo(folders, type=None):
     for folder in folders:
         folds = []
         rows = dao.select("SELECT * FROM Folder WHERE parent=?", (folder.id,))
